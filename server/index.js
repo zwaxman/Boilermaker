3d664cd4a1c9d0cbe67f2ca1d9f2048628c8d156
Zachary Waxman <zwaxman@gmail.com>
2019-10-29 10:59:00 -0500

    Finish React

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname,'..','public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api', require('/api'))
+app.use('/api', require('./api'))
 
 app.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname,'..','index.html'))
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+    entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
+    mode: 'development',
+    output: {
+      path: path.join(__dirname,'public'), // assumes your bundle.js will also be in the root of your project folder
+      filename: 'bundle.js'
+    },
+    devtool: 'source-maps',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    }
+  }
